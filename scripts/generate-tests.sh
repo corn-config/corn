fbcde47b256cc1019396380d12d095785a3a4be0
Jake Stanger <mail@jstanger.dev>
2022-01-28 22:56:45 +0000

    test: add script for generating test output data
    it ain't perfect

---
+++ scripts/generate-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+export CORN_TEST=foobar
+
+rm -rf assets/outputs
+mkdir -p assets/outputs/{json,yaml,toml}
+
+for file in assets/inputs/*; do
+  basename=$(basename "$file" .corn)
+
+  echo "$basename"
+
+  cargo run -- "$file" -t json > assets/outputs/json/"$basename".json
+  cargo run -- "$file" -t yaml > assets/outputs/yaml/"$basename".yml
+  cargo run -- "$file" -t toml > assets/outputs/toml/"$basename".toml
+done
