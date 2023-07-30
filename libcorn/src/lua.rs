848f70655a9a6ca6513e7d6e6dead1033ff1d7f6
A-Cloud-Ninja <5809177+A-Cloud-Ninja@users.noreply.github.com>
2023-07-30 10:14:15 -0400

    feat(libcorn): lua support via library export
    Introduces the structure for a Lua process to load libcorn.so

---
+++ libcorn/src/lua.rs
@@ -0,0 +1,24 @@
+use crate::Value;
+use mlua::prelude::*;
+
+impl<'lua> ToLua<'lua> for Value<'lua> {
+    fn to_lua(self, lua: &'lua Lua) -> LuaResult<LuaValue<'lua>> {
+        lua.to_value(&self)
+    }
+}
+
+fn lua_parse(lua: &Lua, config: String) -> LuaResult<LuaValue> {
+    let res = crate::parse(&config);
+    match res {
+        Ok(v) => Ok(lua.to_value(&v)?),
+        Err(e) => Err(LuaError::RuntimeError(e.to_string())),
+    }
+}
+
+#[mlua::lua_module]
+fn libcorn(lua: &Lua) -> LuaResult<LuaTable> {
+    let exports = lua.create_table()?;
+    let parse = lua.create_function(lua_parse)?;
+    exports.set("parse", parse)?;
+    Ok(exports)
+}
